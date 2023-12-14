0fab602e9429b817ccd6e56dd326ad890e94d572
Will <93052902+ACIDULATED@users.noreply.github.com>
2023-12-14 13:25:09 -0500

    Add player movement

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include "constants.h"
+#include <stdbool.h>
 
 
 Image crash;
 Image ps1;
-//Image guardian;
+Image guardianWalk;
+Image guardian;
 
 int x = 0;
 int y = 0;
-int speed = 4;
+int speed = 2;
+
+bool walking = false;
 
 int main() {
 	initialize();
@@ -26,34 +30,53 @@ void initialize() {
 	setBackgroundColor(createColor(0, 0, 0));
 	ps1 = createImage(img_ps1);
 	crash = createImage(img_crash);
-	//guardian = createImage(img_guardian);
+	guardianWalk = createImage(img_crash);
+	guardianWalk = moveImage(guardianWalk, 128, 112);
+	guardian = createImage(img_ps1);
+	guardian = moveImage(guardian, 128, 112);
 }
 
 void update() {
 	padUpdate();
 	if(padCheck(Pad1Up)) {
-		y -= speed;
+		y += speed;
 		crash = moveImage(crash, x, y);
+		ps1 = moveImage(ps1, x, y);
 	}
 
 	if(padCheck(Pad1Down)) {
-		y += speed;
+		y -= speed;
 		crash = moveImage(crash, x, y);
+		ps1 = moveImage(ps1, x, y);
 	}
 
 	if(padCheck(Pad1Left)) {
-		x -= speed;
+		x += speed;
 		crash = moveImage(crash, x, y);
+		ps1 = moveImage(ps1, x, y);
 	}
 
 	if(padCheck(Pad1Right)) {
-		x += speed;
+		x -= speed;
 		crash = moveImage(crash, x, y);
+		ps1 = moveImage(ps1, x, y);
+	}
+
+	if (padCheck(Pad1Right) || padCheck(Pad1Left) || padCheck(Pad1Up) || padCheck(Pad1Down)) {
+		walking = true;
+	}
+	else {
+		walking = false;
 	}
 }
 
 void draw() {
+	if (walking) {
+		drawImage(guardianWalk);
+	}
+	else {
+		drawImage(guardian);
+	}
 	drawImage(crash);
 	drawImage(ps1);
-	//drawImage(guardian);
 }
